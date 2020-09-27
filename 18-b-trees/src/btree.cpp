bcbb2a8fbfc4550617e02ba72a5fb264cfbe63c8
Jeongho Jang <josephjang@gmail.com>
2020-09-27 20:54:15 +0900

    Implement BTree::Delete()

--- 18-b-trees/src/btree.cpp
+++ 18-b-trees/src/btree.cpp
@@ -125,7 +125,132 @@ void BTree::SplitChild(Node *node, int index) {
   node->n += 1;
 }
 
-void BTree::Delete(int key) {}
+void BTree::Delete(int key) { DeleteInternal(root, key); }
+
+void BTree::DeleteInternal(Node *node, int key) {
+  if (node->leaf) {
+    std::cout << "delete the key from a leaf: " << node << std::endl;
+    // search key in the node
+    int i = 0;
+    for (; i < node->n; i++) {
+      if (key <= node->keys[i]) {
+        break;
+      }
+    }
+
+    if (i < node->n && key == node->keys[i]) {
+      std::cout << "the key is found at " << node->keys[i] << " in the node "
+                << node << std::endl;
+      for (int j = i; j < node->n - 1; j++) {
+        node->keys[j] = node->keys[j + 1];
+      }
+
+      node->n -= 1;
+    } else {
+      std::cout << "the key is not found" << std::endl;
+    }
+  } else {
+    // search key in the node
+    int i = 0;
+    for (; i < node->n; i++) {
+      if (key <= node->keys[i]) {
+        break;
+      }
+    }
+
+    if (i < node->n && key == node->keys[i]) {
+      // key is in the node
+      std::cout << "found a key: " << node->keys[i] << " in the node " << node
+                << std::endl;
+
+    } else {
+      std::cout << "determined a child: " << i << " (" << node->children[i]
+                << ")" << std::endl;
+      if (node->children[i]->n < t) {
+        // check siblings
+        if (i < node->n && node->children[i + 1]->n >= t) {
+          std::cout << "will execute 3a with the right sibling: "
+                    << node->children[i + 1] << std::endl;
+          auto child = node->children[i];
+          auto sibling = node->children[i + 1];
+          // move a key from the node to the child
+          // assume the child is not the rightmost child
+          child->keys[child->n] = node->keys[i];
+          child->n += 1;
+          // move the leftmost key from the sibling to the node
+          node->keys[i] = sibling->keys[0];
+          // move the leftmost child from the sibling to the child
+          child->children[child->n] = sibling->children[0];
+          for (int j = 0; j < sibling->n - 1; j++) {
+            sibling->keys[j] = sibling->keys[j + 1];
+            sibling->children[j] = sibling->children[j + 1];
+          }
+          sibling->children[sibling->n - 1] = node->children[sibling->n];
+          sibling->n -= 1;
+        } else if (i > 0 && node->children[i - 1]->n >= t) {
+          std::cout << "will execute 3a with the left sibling: "
+                    << node->children[i - 1] << std::endl;
+          auto child = node->children[i];
+          auto sibling = node->children[i - 1];
+          // move a key from the node to the child
+          child->children[child->n + 1] = child->children[child->n];
+          for (int j = child->n - 1; j >= 0; j--) {
+            child->keys[j + 1] = child->keys[j];
+            child->children[j + 1] = child->children[j];
+          }
+          child->keys[0] = node->keys[i - 1];
+          child->n += 1;
+          // move the rightmost key from the sibling to the node
+          node->keys[i - 1] = sibling->keys[sibling->n - 1];
+          // move the rightmost child from the sibling to the child
+          child->children[0] = sibling->children[sibling->n];
+          sibling->n -= 1;
+          Print();
+        } else if (i < node->n && node->children[i + 1]->n < t) {
+          std::cout << "will execute 3b with the right sibling: "
+                    << node->children[i + 1] << std::endl;
+          MergeChildren(node, i);
+        } else if (i > 0 && node->children[i - 1]->n < t) {
+          std::cout << "will execute 3b with the left sibling: "
+                    << node->children[i - 1] << std::endl;
+          MergeChildren(node, i - 1);
+          i = i - 1;
+        }
+      }
+
+      std::cout << "delete the key from a child: " << node->children[i]
+                << std::endl;
+      DeleteInternal(node->children[i], key);
+    }
+  }
+}
+
+void BTree::MergeChildren(Node *node, int index) {
+  // merge the child and the sibling using the parent key as a median
+  auto child = node->children[index];
+  auto sibling = node->children[index + 1];
+  child->keys[child->n] = node->keys[index];
+  child->n += 1;
+  for (int j = 0; j < sibling->n; j++) {
+    child->keys[child->n + j] = sibling->keys[j];
+    child->children[child->n + j] = sibling->children[j];
+  }
+  child->children[child->n + sibling->n] = sibling->children[sibling->n];
+  child->n += sibling->n;
+  // delete the parent key and the children at the right-side
+  for (int j = index; j < node->n - 1; j++) {
+    node->keys[j] = node->keys[j + 1];
+    node->children[j + 1] = node->children[j + 2];
+  }
+  node->n -= 1;
+  if (node->n == 0) {
+    root = child;
+  }
+  // delete the sibling node
+  delete[] sibling->keys;
+  delete[] sibling->children;
+  delete sibling;
+}
 
 void BTree::Print() { PrintNode(root); }
 
--- 18-b-trees/src/btree_test.cpp
+++ 18-b-trees/src/btree_test.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include "btree.h"
 
+const int NUM_ELEMENT = 10;
+
 int main(int argc, char **argv) {
   auto btree = new BTree(3);
   btree->Print();
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < NUM_ELEMENT; i++) {
     btree->Insert(i);
     std::cout << "inserted " << i << std::endl;
-    btree->Print();
+    // btree->Print();
   }
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < NUM_ELEMENT; i++) {
     auto result = btree->Search(i);
     std::cout << std::boolalpha << "searched for " << i << ": " << result
               << std::endl;
+  }
+
+  btree->Print();
+  for (int i = 0; i < 1; i++) {
+    btree->Delete(i);
+    std::cout << "deleted " << i << std::endl;
+    btree->Print();
+  }
+  for (int i = 0; i < 3; i++) {
+    auto index = NUM_ELEMENT - 1 - i;
+    btree->Delete(index);
+    std::cout << "deleted " << index << std::endl;
+    btree->Print();
   }
 
   delete btree;
